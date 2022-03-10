3a272e0fb4d1c3f43536788607c4b1cb3a7b41b5
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-03-10 17:46:29 +0000

    Metodos

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -3,26 +3,29 @@ package caixa;
 public class Application {
 
 	public static void main(String[] args) {
-		
-		
 		// TOPO
+		horizontal();
+
+		// LATERAL
+		vertical();
+
+		//FIM
+		horizontal();
+	}
+	
+	private static void horizontal() {
 		for (int i=0; i < 9; i++) {
 			System.out.print("0");
 		}
 		System.out.println("");
-
-		// LATERAL
+	}
+	
+	private static void vertical() {
 		for (int i=0; i < 9; i++) {
 			System.out.print("0");
 			System.out.print("       ");
 			System.out.print("0");
 			System.out.println("");
 		}
-
-		//FIM
-		for (int i=0; i < 9; i++) {
-			System.out.print("0");
-		}
 	}
-
 }
