ee4aed2fd03ee115917ba6e08e3d1b10ad5ce086
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-03-17 17:19:27 +0000

    Melhorias na caixa

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -3,8 +3,10 @@ package caixa;
 public class Application {
 
 	public static void main(String[] args) {
-		
-		Caixa cx = new Caixa(5, 5);
+		Caixa cx = new Caixa(5, 5, ".");
+		cx.imprimir();
+
+		cx.setCaracter("/");
 		cx.imprimir();
 	}
 }
--- src/caixa/Caixa.java
+++ src/caixa/Caixa.java
@@ -1,38 +1,48 @@
 package caixa;
 
 public class Caixa {
-	
-	private int linhas;
-	private int colunas;
-	
-	
-	public Caixa(int linhas, int colunas) {
-		this.linhas = linhas;
-		this.colunas = colunas;
+
+    private final int linhas;
+    private final int colunas;
+    private String caracter;
+
+    public Caixa(int linhas, int colunas, String caracter) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.caracter = caracter;
+    }
+
+    public void setCaracter(String caracter) {
+        this.caracter = caracter;
+    }
+
+    public void imprimir() {
+        System.out.print(horizontal());
+        System.out.print(vertical());
+		System.out.print(horizontal());
 	}
-	
-	public void imprimir() {
-		horizontal(colunas);
-		vertical(linhas, colunas);
-		horizontal(colunas);
-	}
-	
-	private static void horizontal(int colunas) {
-		for (int i=0; i < colunas; i++) {
-			System.out.print("0");
-		}
-		System.out.println("");
-	}
-	
-	private static void vertical(int linhas, int colunas) {
-		for (int i=0; i < linhas; i++) {
-			System.out.print("0");
-			for (int x=0; x < colunas - 2; x++) {
-				System.out.print(" ");
+
+    public String horizontal() {
+        String resultado = "";
+
+        for (int i = 0; i < colunas; i++) {
+            resultado = resultado.concat(caracter);
+        }
+        resultado = resultado.concat("\n");
+
+        return resultado;
+    }
+
+    private String vertical() {
+		String resultado = "";
+        for (int i = 0; i < linhas; i++) {
+			resultado = resultado.concat(caracter);
+            for (int x = 0; x < colunas - 2; x++) {
+				resultado = resultado.concat(" ");
 			}
-			System.out.print("0");
-			System.out.println("");
-		}
-	}
+			resultado = resultado.concat(caracter+"\n");
+        }
+		return resultado;
+    }
 
 }
