93d074aef5f98573b01558d959c7db4b9645bd23
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-03-17 18:03:09 +0000

    Add Javadoc

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -4,9 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		Caixa cx = new Caixa(5, 5, ".");
-		cx.imprimir();
+//		cx.imprimir();
+		System.out.println(cx);
 
-		cx.setCaracter("/");
-		cx.imprimir();
+//		cx.setCaracter("/");
+//		cx.imprimir();
 	}
 }
--- src/caixa/Caixa.java
+++ src/caixa/Caixa.java
@@ -1,9 +1,29 @@
 package caixa;
 
+/**
+ * <h1>Classe caixa</h1>
+ *
+ * <p>Imprime uma caixa no ecrã com o numero de linhas e colunas definidos no construtor</p>
+ *
+ * @author Bruno Jesus
+ * @version 1.0
+ * @since 2022-03-17
+ */
 public class Caixa {
 
+    /**
+     * Numero de linhas da caixa
+     */
     private final int linhas;
+
+    /**
+     * Numero de colunas da caixa
+     */
     private final int colunas;
+
+    /**
+     * Numero de caracteres da caixa
+     */
     private String caracter;
 
     public Caixa(int linhas, int colunas, String caracter) {
@@ -12,17 +32,40 @@ public class Caixa {
         this.caracter = caracter;
     }
 
+    /**
+     * Muda o caracter a ser impresso para a caixa
+     */
     public void setCaracter(String caracter) {
         this.caracter = caracter;
     }
 
+    /**
+     * Imprime a caixa no ecrã
+     *
+     * @see System#out
+     * @see Caixa#toString()
+     * @deprecated use {@link Caixa#toString()} instead
+     */
     public void imprimir() {
-        System.out.print(horizontal());
+        System.out.print(horizontal()); //horizontal
         System.out.print(vertical());
 		System.out.print(horizontal());
 	}
 
-    public String horizontal() {
+    /**
+     * Representação da caixa em String
+     * @return String a caixa a ser desenhada
+     */
+    public String toString() {
+        String resultado = "";
+        resultado = resultado.concat(horizontal());
+        resultado = resultado.concat(vertical());
+        resultado = resultado.concat(horizontal());
+
+        return resultado;
+    }
+
+    private String horizontal() {
         String resultado = "";
 
         for (int i = 0; i < colunas; i++) {
@@ -35,8 +78,10 @@ public class Caixa {
 
     private String vertical() {
 		String resultado = "";
+
         for (int i = 0; i < linhas; i++) {
 			resultado = resultado.concat(caracter);
+            // ciclo para as colunas
             for (int x = 0; x < colunas - 2; x++) {
 				resultado = resultado.concat(" ");
 			}
